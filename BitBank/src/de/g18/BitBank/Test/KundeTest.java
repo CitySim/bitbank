814acb0d6c6541d0a31b73eb54e2ae07aa01b73a
it1-korebj <it1-korebj@R053-07.g18.de>
2013-04-11 14:02:11 +0200

    Kunde-Test

--- BitBank/src/de/g18/BitBank/Test/KundeTest.java
+++ BitBank/src/de/g18/BitBank/Test/KundeTest.java
@@ -20,77 +20,16 @@ public class KundeTest {
 	/**
 	 * @throws java.lang.Exception
 	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@Before
 	public void setUp() throws Exception {
 	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	
 	/**
 	 * Test method for {@link de.g18.BitBank.Kunde#Kunde(java.lang.String, int)}.
 	 */
 	@Test
 	public void testKunde() {
 		//test
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link de.g18.BitBank.Kunde#getKundenNummmer()}.
-	 */
-	@Test
-	public void testGetKundenNummmer() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link de.g18.BitBank.Kunde#setKundenNummmer(int)}.
-	 */
-	@Test
-	public void testSetKundenNummmer() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link de.g18.BitBank.Kunde#getName()}.
-	 */
-	@Test
-	public void testGetName() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link de.g18.BitBank.Kunde#setName(java.lang.String)}.
-	 */
-	@Test
-	public void testSetName() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link de.g18.BitBank.Kunde#getKontenListe()}.
-	 */
-	@Test
-	public void testGetKontenListe() {
 		fail("Not yet implemented");
 	}
 
