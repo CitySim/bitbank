4bd74300e521a52df053f61e3ff2d28e3f0d4df5
Blub12345 <rocketscienc01100101@gmail.com>
2013-04-11 17:22:12 +0200

    Kunde-Test 1/2

--- BitBank/src/de/g18/BitBank/Test/KundeTest.java
+++ BitBank/src/de/g18/BitBank/Test/KundeTest.java
@@ -5,47 +5,70 @@ package de.g18.BitBank.Test;
 
 import static org.junit.Assert.*;
 
+import java.awt.List;
+import java.util.Arrays;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import de.g18.BitBank.Kontotyp;
+import de.g18.BitBank.Kunde;
+
 /**
  * @author it1-tattsv
- *
+ * 
  */
 public class KundeTest {
+
+	Kunde k;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		k = new Kunde("Karl Kunde", 00001111);
+		k.anlegenKonto(Kontotyp.GIROKONTO);
 	}
-	
+
 	/**
-	 * Test method for {@link de.g18.BitBank.Kunde#Kunde(java.lang.String, int)}.
+	 * Test method for {@link de.g18.BitBank.Kunde#Kunde(java.lang.String, int)}
 	 */
 	@Test
 	public void testKunde() {
-		//test
-		fail("Not yet implemented");
+		assertEquals("Karl Kunde", k.getName());
+		assertEquals(00001111, k.getKundenNummmer());
 	}
 
 	/**
-	 * Test method for {@link de.g18.BitBank.Kunde#anlegenKonto(de.g18.BitBank.Kontotyp)}.
+	 * Test method for
+	 * {@link de.g18.BitBank.Kunde#anlegenKonto(de.g18.BitBank.Kontotyp)}.
 	 */
 	@Test
 	public void testAnlegenKonto() {
-		fail("Not yet implemented");
+		System.out.println(k.getKontenListe());
+		assertTrue(k.getKontenListe().size() == 1);
 	}
 
+	//TODO
 	/**
-	 * Test method for {@link de.g18.BitBank.Kunde#anzeigenKontostandsUebersicht()}.
+	 * Test method for
+	 * {@link de.g18.BitBank.Kunde#anzeigenKontostandsUebersicht()}.
 	 */
 	@Test
 	public void testAnzeigenKontostandsUebersicht() {
+		fail("Not yet implemented");
+	}
+
+	//TODO
+	/**
+	 * Javadoc fehlt
+	 */
+	@Test
+	public void testUeberweisen() {
 		fail("Not yet implemented");
 	}
 
