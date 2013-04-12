35ad55f0287b82a3384d81c9617ebdf837095823
Sven Tatter <sven31894@gmail.com>
2013-04-12 09:02:08 +0200

    halb OK

--- BitBank/src/de/g18/BitBank/Gui/UeberweisungDurchfuehren.java
+++ BitBank/src/de/g18/BitBank/Gui/UeberweisungDurchfuehren.java
@@ -1,6 +1,6 @@
 package de.g18.BitBank.Gui;
 
-import java.awt.GridLayout;
+import java.awt.*;
 
 import javax.swing.*;
 
@@ -8,7 +8,7 @@ import de.g18.BitBank.Gui.Listener.UeberweisungDurchfuehrenListener;
 
 /**
  * Gui Klasse zum Überweisen eines Betrages von einem Konto zum anderen.
- * 
+ *
  * @author it1-markde
  * @since JRE6
  */
@@ -16,52 +16,89 @@ import de.g18.BitBank.Gui.Listener.UeberweisungDurchfuehrenListener;
 @SuppressWarnings("serial")
 public class UeberweisungDurchfuehren extends JFrame {
 
-	public UeberweisungDurchfuehren() {
+    public UeberweisungDurchfuehren() {
 
-		this.setSize(400, 400);
-		this.setLocationRelativeTo(null);
-		this.setLayout(new GridLayout(0, 1));
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("Bank-Anwendung - Überweisung durchführen");
+        this.setSize(400, 400);
+        this.setLocationRelativeTo(null);
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(2, 2, 2, 2);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Bank-Anwendung - Überweisung durchführen");
 
-		JLabel vomKontoLabel = new JLabel("Vom Konto");
-		JLabel nachKontoLabel = new JLabel("Nach Konto");
-		JLabel datumLabel = new JLabel("Datum");
-		JLabel betragLabel = new JLabel("Betrag");
+        JLabel vomKontoLabel = new JLabel("Vom Konto");
+        JLabel nachKontoLabel = new JLabel("Nach Konto");
+        JLabel datumLabel = new JLabel("Datum");
+        JLabel betragLabel = new JLabel("Betrag");
 
-		JTextField vomKontoField = new JTextField(10);
-		JTextField nachKontoField = new JTextField(10);
-		JTextField datumField = new JTextField(10);
-		JTextField betragField = new JTextField(10);
+        JTextField vomKontoField = new JTextField(10);
+        JTextField nachKontoField = new JTextField(10);
+        JTextField datumField = new JTextField(10);
+        datumField.setEnabled(false);
+        JTextField betragField = new JTextField(10);
 
-		JButton datumAuswählenButton = new JButton("Datum auswählen");
-		JButton überweisenButton = new JButton("Überweisen");
-		JButton beendenButton = new JButton("Beenden");
+        JButton datumAuswählenButton = new JButton("Datum auswählen");
+        JButton überweisenButton = new JButton("Überweisen");
+        JButton beendenButton = new JButton("Beenden");
 
-		this.add(vomKontoLabel);
-		this.add(nachKontoLabel);
-		this.add(datumLabel);
-		this.add(betragLabel);
+        c.gridx = 0;
+        c.gridy = 0;
+        this.add(vomKontoLabel, c);
 
-		this.add(vomKontoField);
-		this.add(nachKontoField);
-		this.add(datumField);
-		this.add(betragField);
+        c.gridx = 0;
+        c.gridy = 1;
+        this.add(nachKontoLabel, c);
 
-		this.add(datumAuswählenButton);
-		this.add(überweisenButton);
-		this.add(beendenButton);
+        c.gridx = 0;
+        c.gridy = 2;
+        this.add(datumLabel, c);
 
-		datumAuswählenButton
-				.addActionListener(new UeberweisungDurchfuehrenListener());
-		überweisenButton
-				.addActionListener(new UeberweisungDurchfuehrenListener());
-		beendenButton.addActionListener(new UeberweisungDurchfuehrenListener());
+        c.gridx = 0;
+        c.gridy = 3;
+        this.add(betragLabel, c);
 
-		this.setVisible(true);
-	}
 
-	public static void main(String[] args) {
+        c.gridx = 1;
+        c.gridy = 0;
+        c.weightx = 1;
+        this.add(vomKontoField, c);
+
+        c.gridx = 1;
+        c.gridy = 1;
+        this.add(nachKontoField, c);
+
+        c.gridx = 1;
+        c.gridy = 2;
+        this.add(datumField, c);
+
+        c.gridx = 1;
+        c.gridy = 3;
+        this.add(betragField, c);
+
+        c.weightx = 0;
+        c.gridx = 2;
+        c.gridy = 2;
+        this.add(datumAuswählenButton, c);
+
+        c.gridx = 2;
+        c.gridy = 3;
+        this.add(überweisenButton, c);
+
+        c.gridx = 2;
+        c.gridy = 4;
+        this.add(beendenButton, c);
+
+        datumAuswählenButton
+                .addActionListener(new UeberweisungDurchfuehrenListener());
+        überweisenButton
+                .addActionListener(new UeberweisungDurchfuehrenListener());
+        beendenButton.addActionListener(new UeberweisungDurchfuehrenListener());
+
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
@@ -69,6 +106,6 @@ public class UeberweisungDurchfuehren extends JFrame {
         }
         //test
 
-		new UeberweisungDurchfuehren();
-	}
+        new UeberweisungDurchfuehren();
+    }
 }
