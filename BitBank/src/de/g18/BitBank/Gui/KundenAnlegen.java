dca1f7503873e2038ebdc1b8c49e7cf94f1a2011
Blub12345 <rocketscienc01100101@gmail.com>
2013-04-13 16:15:38 +0200

    KundenAnlegen GUI: Eingabebereich fertig, Buttons müssen noch angepasst werden

--- BitBank/src/de/g18/BitBank/Gui/KundenAnlegen.java
+++ BitBank/src/de/g18/BitBank/Gui/KundenAnlegen.java
@@ -1,10 +1,15 @@
 package de.g18.BitBank.Gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import de.g18.BitBank.Gui.Listener.KundenAnlegenListener;
@@ -20,27 +25,37 @@ import de.g18.BitBank.Gui.Listener.KundenAnlegenListener;
 public class KundenAnlegen extends JFrame {
 	public KundenAnlegen() {
 
-		this.setSize(400, 400);
+		this.setSize(450, 200);
 		this.setLocationRelativeTo(null);
 		this.setLayout(new GridLayout(0, 1));
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setTitle("Bank-Anwendung - Kunde anlegen");
 
+		JPanel eingabe = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel buttons = new JPanel();
 		JLabel kundenNummerLabel = new JLabel("Kundennummer");
 		JLabel kundenNamenLabel = new JLabel("Kundenname");
-		JTextField kundenNummerField = new JTextField(10);
-		JTextField kundenNamenField = new JTextField(10);
+		JTextField kundenNummerField = new JTextField(20);
+		JTextField kundenNamenField = new JTextField(20);
 		JButton anlegenButton = new JButton("Anlegen");
 		JButton beendenButton = new JButton("Beenden");
-
-		this.add(kundenNummerLabel);
-		this.add(kundenNamenLabel);
-
-		this.add(kundenNamenField);
-		this.add(kundenNummerField);
-
-		this.add(anlegenButton);
-		this.add(beendenButton);
+		
+		Dimension labelSize = new Dimension(145, 25);
+		kundenNummerLabel.setPreferredSize(labelSize);
+		kundenNamenLabel.setPreferredSize(labelSize);
+		eingabe.setBorder(BorderFactory.createMatteBorder(20, 25, 0, 25,
+				Color.decode("#EEEEEE")));
+		buttons.setBorder(BorderFactory.createMatteBorder(20, 20, 20, 20,
+				Color.decode("#EEEEEE")));
+		
+		this.add(eingabe);
+		this.add(buttons);
+		eingabe.add(kundenNummerLabel);
+		eingabe.add(kundenNummerField);
+		eingabe.add(kundenNamenLabel);
+		eingabe.add(kundenNamenField);
+		buttons.add(anlegenButton);
+		buttons.add(beendenButton);
 
 		anlegenButton.addActionListener(new KundenAnlegenListener(
 				kundenNummerField, kundenNamenField));
