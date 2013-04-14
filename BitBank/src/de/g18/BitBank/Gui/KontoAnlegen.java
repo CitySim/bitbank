3f4f179822b0e6c7eae930c30a263767199bcd8e
Sven Tatter <sven31894@gmail.com>
2013-04-14 12:38:54 +0200

    GrifBag für Konto anlegen

--- BitBank/src/de/g18/BitBank/Gui/KontoAnlegen.java
+++ BitBank/src/de/g18/BitBank/Gui/KontoAnlegen.java
@@ -18,42 +18,61 @@ public class KontoAnlegen extends JPanel {
 	public KontoAnlegen(JTabbedPane tabsPane) {
 		this.tabsPane = tabsPane;
 
-		this.setLayout(new GridLayout(0, 3));
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.insets = new Insets(5, 5, 5, 5);
 
-		JPanel links = new JPanel();
-		JPanel mitte = new JPanel();
-		JPanel rechts = new JPanel();
-		JLabel kundenNummerLabel = new JLabel("KundenNummer");
-		JTextField kundenNummerField = new JTextField(10);
+		JLabel kundenNummerLabel = new JLabel("Kundennummer");
+		JTextField kundenNummerField = new JTextField();
+		JLabel KontotypLabel = new JLabel("Kontotyp");
 		JRadioButton giroKontoRadioButton = new JRadioButton("GiroKonto");
 		JRadioButton sparKontoRadioButton = new JRadioButton("SparKonto");
 		JButton anlegenButton = new JButton("Anlegen");
 		JButton beendenButton = new JButton("Beenden");
 
-		mitte.setLayout(new FlowLayout(FlowLayout.LEFT));
-		anlegenButton.setPreferredSize(new Dimension(100, 25));
-		beendenButton.setPreferredSize(new Dimension(100, 25));
-		links.setBorder(BorderFactory.createMatteBorder(20, 0, 0, 0,
-				Color.decode("#EEEEEE")));
-		mitte.setBorder(BorderFactory.createMatteBorder(20, 0, 0, 0,
-				Color.decode("#EEEEEE")));
-		rechts.setBorder(BorderFactory.createMatteBorder(75, 0, 0, 0,
-				Color.decode("#EEEEEE")));
+		ButtonGroup kontoTypSelect = new ButtonGroup();
+		kontoTypSelect.add(giroKontoRadioButton);
+		kontoTypSelect.add(sparKontoRadioButton);
 
-		this.add(links);
-		this.add(mitte);
-		this.add(rechts);
-		links.add(kundenNummerLabel);
-		mitte.add(kundenNummerField);
-		mitte.add(giroKontoRadioButton);
-		mitte.add(sparKontoRadioButton);
-		rechts.add(anlegenButton);
-		rechts.add(beendenButton);
+		c.gridx = 0;
+		c.gridy = 0;
+		add(kundenNummerLabel, c);
+
+		c.gridx = 1;
+		c.gridy = 0;
+		c.weightx = 1;
+		add(kundenNummerField, c);
+
+		c.gridx = 0;
+		c.gridy = 1;
+		c.weightx = 0;
+		add(KontotypLabel, c);
+
+		c.gridx = 1;
+		c.gridy = 1;
+		add(giroKontoRadioButton, c);
+
+		c.gridx = 1;
+		c.gridy = 2;
+		add(sparKontoRadioButton, c);
+
+		c.gridx = 2;
+		c.gridy = 2;
+		add(anlegenButton, c);
+
+		c.gridx = 2;
+		c.gridy = 3;
+		add(beendenButton, c);
+
+		c.gridx = 0;
+		c.gridy = 4;
+		c.gridwidth = 3;
+		c.weighty = 1;
+		this.add(new JPanel(), c);
 
 		anlegenButton.addActionListener(new KontoAnlegenListener(
 				kundenNummerField));
 		beendenButton.addActionListener(new KontoAnlegenListener(this));
-
-		this.setVisible(true);
 	}
 }
