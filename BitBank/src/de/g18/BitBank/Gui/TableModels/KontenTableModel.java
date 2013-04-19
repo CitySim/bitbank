c245a14529326d56ed60f315be9aab959ee40cbd
Sven Tatter <sven31894@gmail.com>
2013-04-19 16:45:58 +0200

    kontenliste, sparkonto => spar, girokonto => giro

--- BitBank/src/de/g18/BitBank/Gui/TableModels/KontenTableModel.java
+++ BitBank/src/de/g18/BitBank/Gui/TableModels/KontenTableModel.java
@@ -1,9 +1,9 @@
 package de.g18.BitBank.Gui.TableModels;
 
-import javax.swing.table.AbstractTableModel;
-
 import de.g18.BitBank.Konto;
 import de.g18.BitBank.Kunde;
+
+import javax.swing.table.AbstractTableModel;
 
 /**
  * Created with IntelliJ IDEA. User: Sven Date: 17.04.13 Time: 22:38
@@ -11,7 +11,7 @@ import de.g18.BitBank.Kunde;
 public class KontenTableModel extends AbstractTableModel {
 	private static final long serialVersionUID = -3758449082711896808L;
 	private Kunde kunde;
-	private String[] cols = { "Kontoart", "Kontonummer", "Kontostand" };
+	private String[] cols = {"Kontoart", "Kontonummer", "Kontostand"};
 
 	public KontenTableModel(final Kunde kunde) {
 		this.kunde = kunde;
@@ -37,14 +37,21 @@ public class KontenTableModel extends AbstractTableModel {
 		Konto k = kunde.getKontenListe().get(rowIndex);
 
 		switch (columnIndex) {
-		case 0:
-			return k.getKontoTyp().toString();
-		case 1:
-			return Long.toString(k.getKontoNummer());
-		case 2:
-			return Double.toString(k.getKontoStand());
-		default:
-			return "Fehler";
+			case 0:
+				switch (k.getKontoTyp()) {
+					case GIROKONTO:
+						return "GIRO";
+					case SPARKONTO:
+						return "SPAR";
+					default:
+						return "UNBEKANNT";
+				}
+			case 1:
+				return Long.toString(k.getKontoNummer());
+			case 2:
+				return Double.toString(k.getKontoStand());
+			default:
+				return "Fehler";
 		}
 	}
 }
