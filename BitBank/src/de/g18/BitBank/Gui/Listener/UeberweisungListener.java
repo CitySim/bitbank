5257bdbf2b805945aa8fa720dcb4f454eccca830
it1-markde <it1-markde@R053-11.g18.de>
2013-04-19 10:50:37 +0200

    Whatever.

--- BitBank/src/de/g18/BitBank/Gui/Listener/UeberweisungListener.java
+++ BitBank/src/de/g18/BitBank/Gui/Listener/UeberweisungListener.java
@@ -4,6 +4,7 @@ import com.toedter.calendar.JDateChooser;
 import de.g18.BitBank.BankController;
 import de.g18.BitBank.Exception.BetragNegativException;
 import de.g18.BitBank.Exception.KontoLeerException;
+import de.g18.BitBank.Exception.KontoNichtGefundenException;
 import de.g18.BitBank.Gui.Ueberweisung;
 
 import javax.swing.*;
@@ -11,9 +12,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Date;
 
-/**F
- * Listener zu den Buttons der Ueberweisung Klasse.
- *
+/**
+ * F Listener zu den Buttons der Ueberweisung Klasse.
+ * 
  * @author it1-markde
  * @since JRE6
  */
@@ -26,14 +27,13 @@ public class UeberweisungListener implements ActionListener {
 	private BankController controller;
 	private JDateChooser chooser;
 
-	public UeberweisungListener(
-			Ueberweisung ueberweisungFrame) {
+	public UeberweisungListener(Ueberweisung ueberweisungFrame) {
 		this.ueberweisungFrame = ueberweisungFrame;
 	}
 
 	public UeberweisungListener(JTextField vomKontoField,
-								JTextField nachKontoField, JTextField betragField,
-								BankController controller, JDateChooser chooser) {
+			JTextField nachKontoField, JTextField betragField,
+			BankController controller, JDateChooser chooser) {
 
 		this.vomKontoField = vomKontoField;
 		this.nachKontoField = nachKontoField;
@@ -56,27 +56,38 @@ public class UeberweisungListener implements ActionListener {
 			try {
 				betrag = Double.parseDouble(this.betragField.getText());
 				vomKontoNummer = Integer.parseInt(this.vomKontoField.getText());
-				nachKontoNummer = Integer.parseInt(this.nachKontoField.getText());
+				nachKontoNummer = Integer.parseInt(this.nachKontoField
+						.getText());
 			} catch (Exception e) {
-				JOptionPane.showMessageDialog(null, "Bitee prüfen zahl eingabe dU§$", "zHAl", JOptionPane.OK_OPTION);
+				JOptionPane.showMessageDialog(null,
+						"Bitee prüfen zahl eingabe dU§$", "zHAl",
+						JOptionPane.OK_OPTION);
 				return;
 			}
 
 			Date datum = chooser.getDate();
 
 			try {
-				this.controller.ueberweisen(nachKontoNummer, vomKontoNummer, betrag, datum);
+				this.controller.ueberweisen(nachKontoNummer, vomKontoNummer,
+						betrag, datum);
 			} catch (KontoLeerException e) {
-				JOptionPane.showMessageDialog(null, "Konto leer:", "Fehler", JOptionPane.OK_OPTION);
+				JOptionPane.showMessageDialog(null, "Konto leer:", "Fehler",
+						JOptionPane.OK_OPTION);
 				return;
 			} catch (BetragNegativException e) {
-				JOptionPane.showMessageDialog(null, "betrag nicht negativvvröjrea", "Fehler", JOptionPane.OK_OPTION);
+				JOptionPane.showMessageDialog(null,
+						"betrag nicht negativvvröjrea", "Fehler",
+						JOptionPane.OK_OPTION);
+				return;
+			} catch (KontoNichtGefundenException e) {
+				JOptionPane.showMessageDialog(null,
+						"betrag nicht negativvvröjrea", "Fehler",
+						JOptionPane.OK_OPTION);
 				return;
 			}
 
 		} else if (buttonClicked.getText().compareTo("Beenden") == 0) {
-			this.ueberweisungFrame.getTabsPane().remove(
-					this.ueberweisungFrame);
+			this.ueberweisungFrame.getTabsPane().remove(this.ueberweisungFrame);
 		}
 	}
 
