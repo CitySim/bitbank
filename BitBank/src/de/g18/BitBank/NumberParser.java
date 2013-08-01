e9ac958e1775f663bb9ce3be15b004aa8b9733fa
it1-markde <it1-markde@R053-11.g18.de>
2013-08-01 09:13:07 +0200

    Test

--- BitBank/src/de/g18/BitBank/NumberParser.java
+++ BitBank/src/de/g18/BitBank/NumberParser.java
@@ -4,7 +4,7 @@ import de.g18.BitBank.Exception.BetragZuGrossException;
 import de.g18.BitBank.Exception.ZuVieleNachkommastellenException;
 
 /**
- * Hilfsklasse zum parsen von String zu double.
+ * Hilfsklasse zum Parsen von String zu double.
  * 
  * @author it1-korebj
  * @since jdk1.7.0_17
@@ -12,42 +12,51 @@ import de.g18.BitBank.Exception.ZuVieleNachkommastellenException;
 
 public class NumberParser {
 
-    /**
-     * Ermöglicht das Umwandeln vom Komma separierten Zahlen.
-     * 
-     * @param text eingelesener Text
-     * @return betrag - Summe als double
-     * @throws ZuVieleNachkommastellenException Exception bei Zahlen mit zu vielen Nachkommastellen
-     * @throws BetragZuGroßException
-     */
-    public final double parseDouble(String text) throws ZuVieleNachkommastellenException, BetragZuGrossException {
+	/**
+	 * Ermöglicht das Umwandeln vom Komma separierten Zahlen.
+	 * 
+	 * @param text
+	 *            eingelesener Text
+	 * @return betrag - Summe als double
+	 * @throws ZuVieleNachkommastellenException
+	 *             Exception bei Zahlen mit zu vielen Nachkommastellen
+	 * @throws BetragZuGroßException
+	 */
+	public final double parseDouble(String text)
+			throws ZuVieleNachkommastellenException, BetragZuGrossException {
 
-        if (text.contains(",")) {
-            text = (text.substring(0, text.indexOf(",")) + "." + (text.substring(text.indexOf(",") + 1)));
-        }
+		if (text.contains(",")) {
+			text = (text.substring(0, text.indexOf(",")) + "." + (text
+					.substring(text.indexOf(",") + 1)));
+		}
 
-        final double betrag = Double.parseDouble(text);
+		final double betrag = Double.parseDouble(text);
 
-        if (betrag > Long.MAX_VALUE) {
-            throw new BetragZuGrossException("Betrag zu groß");
-        }
+		if (betrag > Long.MAX_VALUE) {
+			throw new BetragZuGrossException("Betrag zu groß");
+		}
 
-        this.checkNumber(betrag);
-        return betrag;
-    }
+		this.checkNumber(betrag);
+		return betrag;
+	}
 
-    /**
-     * Fuehrt eine Kontrolle auf zahlen mit unlogisch vielen Nachkommastellen durch.
-     * 
-     * @param betrag Summe als double
-     * @throws ZuVieleNachkommastellenException Exception bei Zahlen mit zu vielen Nachkommastellen
-     */
-    private void checkNumber(final double betrag) throws ZuVieleNachkommastellenException {
+	/**
+	 * Fuehrt eine Kontrolle auf zahlen mit unlogisch vielen Nachkommastellen
+	 * durch.
+	 * 
+	 * @param betrag
+	 *            Summe als double
+	 * @throws ZuVieleNachkommastellenException
+	 *             Exception bei Zahlen mit zu vielen Nachkommastellen
+	 */
+	private void checkNumber(final double betrag)
+			throws ZuVieleNachkommastellenException {
 
-        final long l = (long) (betrag * 100.0);
+		final long l = (long) (betrag * 100.0);
 
-        if (l != betrag * 100) {
-            throw new ZuVieleNachkommastellenException("Anzahl Nachkommastellen: ");
-        }
-    }
+		if (l != betrag * 100) {
+			throw new ZuVieleNachkommastellenException(
+					"Anzahl Nachkommastellen: ");
+		}
+	}
 }
